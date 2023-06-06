83be072fe11e17e67d6157f47d5e37e88a592a27
Ravictation <125954517+Ravictation@users.noreply.github.com>
2023-06-06 11:07:51 +0800

    Delete tes.js

--- tes.js
+++
@@ -1,9 +0,0 @@
-const arrayAngka = ['595656', '159466', '56'];
-
-// Mengurutkan angka dalam setiap indeks array secara ascending
-arrayAngka.forEach((elemen, indeks) => {
-  const angkaTerurut = elemen.split('').sort((a, b) => parseInt(a) - parseInt(b)).join('');
-  arrayAngka[indeks] = angkaTerurut;
-});
-
-console.log("Angka dalam setiap indeks array terurut: ", arrayAngka);
