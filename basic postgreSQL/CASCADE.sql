7f30b8e3debba6efeb4de8b9286441fe680c7aa8
Ravictation <125954517+Ravictation@users.noreply.github.com>
2023-06-06 11:10:53 +0800

    Delete basic postgreSQL directory

--- basic postgreSQL/CASCADE.sql
+++
@@ -1,8 +0,0 @@
--- ALTER TABLE Booking
--- DROP CONSTRAINT fk_schedule;
-
--- ALTER TABLE Booking
--- ADD CONSTRAINT fk_schedule
--- FOREIGN KEY (schedule_id) 
--- REFERENCES Schedule(schedule_id)
--- ON DELETE CASCADE;
--- basic postgreSQL/query_name.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM Movie 
-    WHERE title iLIKE '%a%'
-    ORDER BY movie_id ASC
-    LIMIT 5
-    OFFSET 0 ;
